9a84d34783a0862880175885e22ef14f123d8960
Joe Orr <joe.orr@gmail.com>
2016-04-05 17:38:55 -0400

    dist

---
+++ .tmp/app.module.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/* commonjs package manager support (eg. componentjs) */
+if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports) {
+    module.exports = 'color.selectbox';
+}
+
+(function () {
+    'use strict';
+
+    angular.module('color.selectbox', []);
+})();
+//# sourceMappingURL=app.module.js.map
